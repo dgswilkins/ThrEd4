5ebc9064418f1352076657f0448701bf1db966e5
Douglas Wilkins <dgswilkins@users.noreply.github.com>
2018-03-06 14:12:29 -0500

    fix satfn failing on fan fills

--- form.cpp
+++ form.cpp
@@ -3176,11 +3176,12 @@ void durgn(std::vector<FSEQ> &sequencePath, boost::dynamic_bitset<> &visitedRegi
 	unsigned	seql = 0, seqn = 0;
 	unsigned	sequenceStart = 0;
 	unsigned	sequenceEnd = 0;
-	const unsigned	nextGroup = sequencePath[pthi].nextGroup;
 	unsigned	groupStart = 0, groupEnd = 0;
-	unsigned	iRegion = sequencePath[pthi].node;
 	double		length = 0.0, minimumLength = 0.0;
 	BSEQPNT*	bpnt = nullptr;
+	const unsigned	nextGroup = sequencePath[pthi].nextGroup;
+	const unsigned	iRegion = sequencePath[pthi].node;
+
 	boost::dynamic_bitset<> sequenceMap(lineCount);
 
 	//ToDo - More renaming required
@@ -3691,7 +3692,7 @@ void lcon(std::vector<unsigned> &groupIndexSequence, std::vector<SMALPNTL> &line
 			count = 0xffffffff;
 			sequencePath.reserve(SequencePathIndex);
 			for (iPath = 0; iPath < SequencePathIndex; iPath++) {
-				bool tmpSkip = tempPath[iPath].skp;
+				const bool tmpSkip = tempPath[iPath].skp;
 				unsigned short tmpNode = 0;
 				if (tempPath[iPath].pcon == 0xffffffff) {
 					tmpNode = tempPath[iPath].count;
@@ -4628,15 +4629,16 @@ void satfn(unsigned line1Start, unsigned line1End, unsigned line2Start, unsigned
 		line1Segments = ((line1End > line1Start) ? (line1End - line1Start) : (line1Start - line1End));
 		line2Segments = ((line2Start > line2End) ? (line2Start - line2End) : (line2End - line2Start));
 		std::vector<unsigned> line1StitchCounts;
-		line1StitchCounts.reserve(line1Segments + 1);
+		line1StitchCounts.reserve(line1Segments);
 		std::vector<unsigned> line2StitchCounts;
-		line2StitchCounts.reserve(line2Segments + 1);
+		line2StitchCounts.reserve(line2Segments);
 		iVertex = line1Start;
 		segmentStitchCount = 0;
-		for (iSegment = 0; iSegment < line1Segments; iSegment++) {
+		for (iSegment = 0; iSegment < line1Segments - 1; iSegment++) {
 			iNextVertex = nxt(iVertex);
-			line1StitchCounts.push_back(((Lengths[iNextVertex] - Lengths[iVertex]) / line1Length)*stitchCount + 0.5);
-			segmentStitchCount += line1StitchCounts[iSegment];
+			const unsigned val = ((Lengths[iNextVertex] - Lengths[iVertex]) / line1Length)*stitchCount + 0.5;
+			line1StitchCounts.push_back(val);
+			segmentStitchCount += val;
 			iVertex = nxt(iVertex);
 		}
 		line1StitchCounts.push_back(stitchCount - segmentStitchCount);
@@ -4645,8 +4647,9 @@ void satfn(unsigned line1Start, unsigned line1End, unsigned line2Start, unsigned
 		iSegment = 0;
 		segmentStitchCount = 0;
 		while (iVertex > line2End) {
-			line2StitchCounts.push_back(((Lengths[iNextVertex] - Lengths[iVertex]) / line2Length)*stitchCount + 0.5);
-			segmentStitchCount += line2StitchCounts[iSegment++];
+			const unsigned val = ((Lengths[iNextVertex] - Lengths[iVertex]) / line2Length)*stitchCount + 0.5;
+			line2StitchCounts.push_back(val);
+			segmentStitchCount += val;
 			iNextVertex = prv(iNextVertex);
 			iVertex = prv(iNextVertex);
 		}
@@ -4758,7 +4761,7 @@ void satfn(unsigned line1Start, unsigned line1End, unsigned line2Start, unsigned
 					}
 				}
 			}
-			if (iLine1Count < line1Segments) {
+			if ((iLine1Count < line1Segments || iLine2Count < line2Segments)) {
 				if (!line1Count) {
 					line1Count = line1StitchCounts[iLine1Count++];
 					line1Next = nxt(iLine1Vertex);
@@ -4768,16 +4771,16 @@ void satfn(unsigned line1Start, unsigned line1End, unsigned line2Start, unsigned
 					line1Step.x = line1Delta.x / line1Count;
 					line1Step.y = line1Delta.y / line1Count;
 				}
-				if (iLine2Count < line2Segments) {
-					if (!line2Count) {
-						line2Count = line2StitchCounts[iLine2Count++];
-						line2Previous = prv(iLine2Vertex);
-						line2Delta.x = CurrentFormVertices[line2Previous].x - CurrentFormVertices[iLine2Vertex].x;
-						line2Delta.y = CurrentFormVertices[line2Previous].y - CurrentFormVertices[iLine2Vertex].y;
-						iLine2Vertex = prv(iLine2Vertex);
-						line2Step.x = line2Delta.x / line2Count;
-						line2Step.y = line2Delta.y / line2Count;
-					}
+
+				if (!line2Count) {
+					line2Count = line2StitchCounts[iLine2Count++];
+					line2Previous = prv(iLine2Vertex);
+					line2Delta.x = CurrentFormVertices[line2Previous].x - CurrentFormVertices[iLine2Vertex].x;
+					line2Delta.y = CurrentFormVertices[line2Previous].y - CurrentFormVertices[iLine2Vertex].y;
+					iLine2Vertex = prv(iLine2Vertex);
+					line2Step.x = line2Delta.x / line2Count;
+					line2Step.y = line2Delta.y / line2Count;
+
 				}
 				if ((line1Count || line2Count) && line1Count < MAXITEMS && line2Count < MAXITEMS) {
 					flag = true;
