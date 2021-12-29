62faf260004cccfcdd5d696e902c20d1a7c29a64
dgswilkins <dgswilkins@users.noreply.github.com>
2021-12-29 14:58:20 -0800

    use consistent case in EnumMap

--- ThrEd4/EnumMap.h
+++ ThrEd4/EnumMap.h
@@ -8,117 +8,117 @@
 // struct for ensuring an enum has a count element.
 // this does NOT validate that the EnumCount element is the last element
 // default template:
-template <class, class = void> struct has_EnumCount : std::false_type { };
+template <class, class = void> struct hasEnumCount : std::false_type { };
 
-// specialized as has_EnumCount< T , void > or sfinae
+// specialized as hasEnumCount< T , void > or sfinae
 template <class T>
-struct has_EnumCount<T, std::void_t<decltype(T::EnumCount)>> : std::is_same<T, decltype(T::EnumCount)> { };
+struct hasEnumCount<T, std::void_t<decltype(T::EnumCount)>> : std::is_same<T, decltype(T::EnumCount)> { };
 
 template <typename EnumType> class EnumMap
 {
   static_assert(std::is_enum_v<EnumType>, "Type for EnumMap must be an Enum");
-  static_assert(has_EnumCount<EnumType>::value, "Enum provided to EnumMap must have a \"EnumCount\" option as the last element in the enum.");
+  static_assert(hasEnumCount<EnumType>::value, "Enum provided to EnumMap must have a \"EnumCount\" option as the last element in the enum.");
 
   public:
-  explicit constexpr EnumMap(uint32_t i_val) : mask_(i_val) {
+  explicit constexpr EnumMap(uint32_t i_val) : m_mask(i_val) {
   }
   [[nodiscard]] inline auto test(EnumType const i_key) const -> bool {
-	return mask_.test(gsl::narrow_cast<std::underlying_type_t<EnumType>>(i_key));
+	return m_mask.test(gsl::narrow_cast<std::underlying_type_t<EnumType>>(i_key));
   }
   inline void set() {
-	mask_.set();
+	m_mask.set();
   }
   inline void set(EnumType const i_key, bool i_val = true) {
-	mask_.set(gsl::narrow_cast<std::underlying_type_t<EnumType>>(i_key), i_val);
+	m_mask.set(gsl::narrow_cast<std::underlying_type_t<EnumType>>(i_key), i_val);
   }
   inline auto testAndSet(EnumType const i_key, bool i_val = true) -> bool {
-	bool const val = mask_.test(gsl::narrow_cast<std::underlying_type_t<EnumType>>(i_key));
-	mask_.set(gsl::narrow_cast<std::underlying_type_t<EnumType>>(i_key), i_val);
+	bool const val = m_mask.test(gsl::narrow_cast<std::underlying_type_t<EnumType>>(i_key));
+	m_mask.set(gsl::narrow_cast<std::underlying_type_t<EnumType>>(i_key), i_val);
 	return val;
   }
   inline void reset() noexcept {
-	mask_.reset();
+	m_mask.reset();
   }
   inline void reset(EnumType const i_key) {
-	mask_.reset(gsl::narrow_cast<std::underlying_type_t<EnumType>>(i_key));
+	m_mask.reset(gsl::narrow_cast<std::underlying_type_t<EnumType>>(i_key));
   }
   inline auto testAndReset(EnumType const i_key) -> bool {
-	bool const val = mask_.test(gsl::narrow_cast<std::underlying_type_t<EnumType>>(i_key));
-	mask_.reset(gsl::narrow_cast<std::underlying_type_t<EnumType>>(i_key));
+	bool const val = m_mask.test(gsl::narrow_cast<std::underlying_type_t<EnumType>>(i_key));
+	m_mask.reset(gsl::narrow_cast<std::underlying_type_t<EnumType>>(i_key));
 	return val;
   }
   inline void flip() {
-	mask_.flip();
+	m_mask.flip();
   }
   inline void flip(EnumType const i_key) {
-	mask_.flip(gsl::narrow_cast<std::underlying_type_t<EnumType>>(i_key));
+	m_mask.flip(gsl::narrow_cast<std::underlying_type_t<EnumType>>(i_key));
   }
   inline auto testAndFlip(EnumType const i_key) -> bool {
-	bool const val = mask_.test(gsl::narrow_cast<std::underlying_type_t<EnumType>>(i_key));
-	mask_.flip(gsl::narrow_cast<std::underlying_type_t<EnumType>>(i_key));
+	bool const val = m_mask.test(gsl::narrow_cast<std::underlying_type_t<EnumType>>(i_key));
+	m_mask.flip(gsl::narrow_cast<std::underlying_type_t<EnumType>>(i_key));
 	return val;
   }
   inline auto to_ulong() -> uint32_t { // NOLINT(readability-identifier-naming)
-	return mask_.to_ulong();
+	return m_mask.to_ulong();
   }
   [[nodiscard]] inline auto count() const -> size_t {
-	return mask_.count();
+	return m_mask.count();
   } // number of set bits
   [[nodiscard]] inline auto size() const -> size_t {
-	return mask_.size();
+	return m_mask.size();
   } // number of bits in the entire set
   [[nodiscard]] inline auto any() const -> bool {
-	return mask_.any();
+	return m_mask.any();
   }
   [[nodiscard]] inline auto none() const -> bool {
-	return mask_.none();
+	return m_mask.none();
   }
   [[nodiscard]] inline auto all() const -> bool {
-	return mask_.all();
+	return m_mask.all();
   }
   [[nodiscard]] inline auto mask() const
       -> std::bitset<gsl::narrow_cast<std::underlying_type_t<EnumType>>(EnumType::EnumCount)> {
-	return mask_;
+	return m_mask;
   }
   inline void mask(std::bitset<gsl::narrow_cast<std::underlying_type_t<EnumType>>(EnumType::EnumCount)> const& i_mask) {
-	mask_ = i_mask;
+	m_mask = i_mask;
   }
 
   auto operator==(EnumMap<EnumType> const& i_other) const -> bool {
-	return mask_ == i_other.mask_;
+	return m_mask == i_other.m_mask;
   }
   auto operator!=(EnumMap<EnumType> const& i_other) const -> bool {
-	return mask_ != i_other.mask_;
+	return m_mask != i_other.m_mask;
   }
 
   auto operator&=(EnumMap<EnumType> const& i_other) -> EnumMap<EnumType> {
-	mask_ &= i_other.mask_;
+	m_mask &= i_other.m_mask;
 	return *this;
   }
 
   auto operator|=(EnumMap<EnumType> const& i_other) -> EnumMap<EnumType> {
-	mask_ |= i_other.mask_;
+	m_mask |= i_other.m_mask;
 	return *this;
   }
 
   auto operator&(EnumMap<EnumType> const& i_other) const -> EnumMap<EnumType> {
 	EnumMap<EnumType> newMask;
-	newMask.mask_ = mask_ & i_other.mask_;
+	newMask.m_mask = m_mask & i_other.m_mask;
 	return newMask;
   }
 
   auto operator|(EnumMap<EnumType> const& i_other) const -> EnumMap<EnumType> {
 	EnumMap<EnumType> newMask;
-	newMask.mask_ = mask_ | i_other.mask_;
+	newMask.m_mask = m_mask | i_other.m_mask;
 	return newMask;
   }
 
   auto operator~() const -> EnumMap<EnumType> {
 	EnumMap<EnumType> newMask;
-	newMask.mask_ = ~mask_;
+	newMask.m_mask = ~m_mask;
 	return newMask;
   }
 
   private:
-  std::bitset<gsl::narrow_cast<std::underlying_type_t<EnumType>>(EnumType::EnumCount)> mask_;
+  std::bitset<gsl::narrow_cast<std::underlying_type_t<EnumType>>(EnumType::EnumCount)> m_mask;
 };
