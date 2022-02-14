60fc3a65e52fe848abfd1fd677353ceb941c360a
dgswilkins <dgswilkins@users.noreply.github.com>
2022-02-14 11:28:50 -0800

    update header declarations in xt.cpp/h

--- ThrEd4/xt.h
+++ ThrEd4/xt.h
@@ -1,22 +1,12 @@
 #pragma once
 
-#ifndef WIN32_LEAN_AND_MEAN
-#define WIN32_LEAN_AND_MEAN // Exclude rarely-used stuff from Windows headers
-#endif
-
-#ifndef NOMINMAX
-#define NOMINMAX
-#endif
-
-// Windows Header Files:
-#include <Windows.h> // Win32 Platform SDK main header
+// Local Headers
+#include "formHeader.h"
 
 // Standard Libraries
+#include <cstdint>
 #include <filesystem>
 #include <vector>
-
-// Local Headers
-#include "ThrEdTypes.h"
 
 namespace fs = std::filesystem;
 
