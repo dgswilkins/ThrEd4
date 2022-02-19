ec27dbf4608273d0869e55be1192531ff19edb74
dgswilkins <dgswilkins@users.noreply.github.com>
2022-02-19 14:15:13 -0800

    move BACK_HEAD class to thred.cpp

--- ThrEd4/thred.cpp
+++ ThrEd4/thred.cpp
@@ -38,6 +38,32 @@
 //#include <new.h>
 #endif
 
+class BACK_HEAD // Backup header
+{
+  public:
+  uint32_t      formCount {};
+  FRM_HEAD*     forms {};
+  uint32_t      stitchCount {};
+  F_POINT_ATTR* stitches {};
+  uint32_t      vertexCount {};
+  F_POINT*      vertices {};
+  uint32_t      guideCount {};
+  SAT_CON*      guide {};
+  uint32_t      clipPointCount {};
+  F_POINT*      clipPoints {};
+  COLORREF*     colors {};
+  TX_PNT*       texturePoints {};
+  uint32_t      texturePointCount {};
+  SIZE          zoomRect {};
+
+  // constexpr BACK_HEAD() noexcept = default;
+  // BACK_HEAD(CLIP_STITCH const&) = default;
+  // BACK_HEAD(CLIP_STITCH&&) = default;
+  // BACK_HEAD& operator=(BACK_HEAD const& rhs) = default;
+  // BACK_HEAD& operator=(BACK_HEAD&&) = default;
+  //~BACK_HEAD() = default;
+};
+
 #pragma pack(push, 1)
 class BAL_STITCH // balarad stitch
 {
