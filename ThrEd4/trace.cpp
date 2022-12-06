dgswilkins/ThrEd4

commit 314aa4054343df2e987ede3117854993ba7883b1

simplify boolean logic and allow for short-circuited evaluation

ThrEd4/trace.cpp
@@ -644,8 +644,8 @@ auto ti::trcbit(uint32_t const initialDirection, uint32_t& traceDirection, std::
 	  return false;
 	}
   }
-  return !(traceDirection == initialDirection && CurrentTracePoint.x == tracedPoints[0].x &&
-           CurrentTracePoint.y == tracedPoints[0].y);
+  return traceDirection != initialDirection || CurrentTracePoint.x != tracedPoints[0].x ||
+           CurrentTracePoint.y == tracedPoints[0].y;
 }
 
 void ti::dutdif(TRACE_PNT& traceDiff, TRACE_PNT const* point) {

ThrEd4/form.cpp
@@ -1875,7 +1875,7 @@ auto fi::projv(float xCoordinate, F_POINT const& lowerPoint, F_POINT const& uppe
 	if (lower > upper) {
 	  std::swap(lower, upper);
 	}
-	return !(xCoordinate < lower || xCoordinate > upper);
+	return xCoordinate >= lower && xCoordinate <= upper;
   }
   return false;
 }
@@ -1958,11 +1958,11 @@ auto fi::proj(F_POINT const& point, float slope, F_POINT const& point0, F_POINT 
 	  std::swap(yMinimum, yMaximum);
 	}
 	intersectionPoint = intersect;
-	return !(intersect.x <= xMinimum || intersect.x > xMaximum || intersect.y < yMinimum ||
-	         intersect.y > yMaximum);
+	return intersect.x >= xMinimum && intersect.x <= xMaximum && intersect.y >= yMinimum &&
+	         intersect.y <= yMaximum;
   }
   intersectionPoint = intersect;
-  return !(intersect.x <= xMinimum || intersect.x > xMaximum);
+  return intersect.x >= xMinimum && intersect.x <= xMaximum;
 }
 
 auto form::linx(std::vector<F_POINT> const& points, uint32_t start, uint32_t finish, F_POINT& intersection) noexcept
@@ -2007,7 +2007,7 @@ auto fi::projh(float yCoordinate, F_POINT const& point0, F_POINT const& point1, 
   if (leftY > rightY) {
 	std::swap(leftY, rightY);
   }
-  return !(yCoordinate < leftY || yCoordinate > rightY);
+  return yCoordinate >= leftY && yCoordinate <= rightY;
 }
 
 void fi::sprct(std::vector<F_POINT> const* vertices,

ThrEd4/thred.cpp
@@ -16552,7 +16552,7 @@ auto thi::chkMsg(std::vector<POINT>& stretchBoxLine, float& xyRatio, float& angl
 	  }
 #pragma warning(suppress : 26493) // type.4 Don't use C-style casts NOLINTNEXTLINE(cppcoreguidelines-pro-type-cstyle-cast,hicpp-signed-bitwise)
 	  auto const wParameter = LOWORD(Msg.wParam);
-	  if (!(wParameter < ID_FILE_OPEN1 || wParameter > ID_AUXPES)) {
+	  if (wParameter >= ID_FILE_OPEN1 && wParameter <= ID_AUXPES) {
 		thred::undat();
 	  }
 	  if (thi::handleMainMenu(wParameter, rotationCenter)) {

commit 3b4dbcebbc538764da89b05204204aa06214df88

fix error in boolean simplification
This fixes an error introduced in
changeset 314aa405

ThrEd4/trace.cpp
@@ -648,7 +648,7 @@ auto ti::trcbit(uint32_t const initialDirection, uint32_t& traceDirection, std::
 	}
   }
   return traceDirection != initialDirection || CurrentTracePoint.x != tracedPoints[0].x ||
-         CurrentTracePoint.y == tracedPoints[0].y;
+         CurrentTracePoint.y != tracedPoints[0].y;
 }
 
 void ti::dutdif(TRACE_PNT& traceDiff, TRACE_PNT const* point) {