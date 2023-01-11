dgswilkins/ThrEd4

commit 1a235106505462c3aad22e5dcb9a276b52326e3e

Avoid unneccessary writes to variables in DST.cpp

ThrEd4/DST.cpp
@@ -115,9 +115,6 @@ void DST::internal::dstran(std::vector<DSTREC>& DSTData) {
 	color        = colmatch(colors[iColor++]);
 	thred::addColor(0, color);
   }
-  else {
-	color = 0;
-  }
   auto localStitch       = fPOINT {};
   auto maximumCoordinate = fPOINT {-BIGFLOAT, -BIGFLOAT};
   auto mimimumCoordinate = fPOINT {BIGFLOAT, BIGFLOAT};
@@ -232,14 +229,10 @@ void DST::ritdst(DSTOffsets& DSTOffsetData, std::vector<DSTREC>& DSTRecords, std
 	auto       lengths         = POINT {std::lround(stitch.x - wrap::toFloat(centerCoordinate.x)),
                           std::lround(stitch.y - wrap::toFloat(centerCoordinate.y))};
 	auto const absoluteLengths = POINT {abs(lengths.x), abs(lengths.y)};
-	auto       count           = 0;
-	if (absoluteLengths.x > absoluteLengths.y) {
-	  count = absoluteLengths.x / DSTMAX + 1;
-	}
-	else {
-	  count = absoluteLengths.y / DSTMAX + 1;
-	}
-	auto const stepSize = POINT {(absoluteLengths.x / count + 1), (absoluteLengths.y / count + 1)};
+	auto const count = ((absoluteLengths.x > absoluteLengths.y) ? absoluteLengths.x / DSTMAX + 1
+	                                                            : absoluteLengths.y / DSTMAX + 1) +
+	                   1;
+	auto const stepSize = POINT {(absoluteLengths.x / count), (absoluteLengths.y / count)};
 
 	auto difference = POINT {0L, 0L};
 	while ((lengths.x != 0) || (lengths.y != 0)) {

commit 6749372e082934712261c53186e57fcbe1d99e60

fix error in ritdst introduced in 1a235106
When simplifying code by replacing the if construct with a conditional
operator, the division order of operation was not taken into account
properly, so construction of count and stepsize was incorrect.

ThrEd4/DST.cpp
@@ -268,10 +268,9 @@ void DST::ritdst(DST_OFFSETS& DSTOffsetData, std::vector<DSTREC>& DSTRecords, st
 	auto       lengths         = SIZE {std::lround(stitch.x - wrap::toFloat(centerCoordinate.x)),
                          std::lround(stitch.y - wrap::toFloat(centerCoordinate.y))};
 	auto const absoluteLengths = SIZE {abs(lengths.cx), abs(lengths.cy)};
-	auto const count = ((absoluteLengths.cx > absoluteLengths.cy) ? absoluteLengths.cx / DSTMAX + 1
-	                                                              : absoluteLengths.cy / DSTMAX + 1) +
-	                   1;
-	auto const stepSize = SIZE {(absoluteLengths.cx / count), (absoluteLengths.cy / count)};
+	auto const count = (absoluteLengths.cx > absoluteLengths.cy) ? absoluteLengths.cx / DSTMAX + 1
+	                                                              : absoluteLengths.cy / DSTMAX + 1;
+	auto const stepSize = SIZE {(absoluteLengths.cx / count) + 1, (absoluteLengths.cy / count) + 1};
 
 	auto difference = SIZE {};
 	while ((lengths.cx != 0) || (lengths.cy != 0)) {