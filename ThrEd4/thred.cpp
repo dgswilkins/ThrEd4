ce1a8efb1f5a30f855daf23341344e8bd984920f
dgswilkins <dgswilkins@users.noreply.github.com>
2023-02-11 15:02:39 -0800

    unselectAll must also unselect groups of stitches

--- ThrEd4/thred.cpp
+++ ThrEd4/thred.cpp
@@ -10276,6 +10276,7 @@ auto thi::unselectAll() -> bool {
   StateMap->reset(StateFlag::FORMSEL);
   StateMap->reset(StateFlag::FRMPSEL);
   StateMap->reset(StateFlag::FPSEL);
+  StateMap->reset(StateFlag::GRPSEL);
   StateMap->reset(StateFlag::SELBOX);
   StateMap->set(StateFlag::RESTCH);
   thred::unmsg();
