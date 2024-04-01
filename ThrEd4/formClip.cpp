aab01aa9c453429f7eb1d4b88e120fec8267a704
dgswilkins <dgswilkins@users.noreply.github.com>
2024-04-01 12:27:00 -0400

    remove redundant initializer in FORM_CLIP

--- ThrEd4/formClip.cpp
+++ ThrEd4/formClip.cpp
@@ -66,7 +66,7 @@ class FORM_CLIP // form data clipboard header
   public:
   uint32_t clipType {};
   uint32_t reserved {};
-  FRM_HEAD form {};
+  FRM_HEAD form;
 
   // constexpr FORM_CLIP() noexcept = default;
   // FORM_CLIP(FORM_CLIP const&) = default;
