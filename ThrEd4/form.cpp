dgswilkins/ThrEd4

commit decb1b9ccb4f2131f656c6b5b2daa21b9ffe8a17

convert member varibles of D_POINT to private
Add and use getter and setter routines.

ThrEd4/form.cpp
@@ -1972,36 +1972,36 @@ auto fi::proj(F_POINT const& point, float slope, F_POINT const& point0, F_POINT 
     -> bool {
   auto const delta     = D_POINT {point1.x - point0.x, point1.y - point0.y};
   auto       intersect = D_POINT {intersectionPoint};
-  if (delta.x != 0.0) {
-	auto const sideSlope    = delta.y / delta.x;
+  if (delta.getX() != 0.0) {
+	auto const sideSlope    = delta.getY() / delta.getX();
 	auto const sideConstant = wrap::toDouble(point0.y) - sideSlope * wrap::toDouble(point0.x);
 	auto const pointConstant =
 	    wrap::toDouble(point.y) - wrap::toDouble(slope) * wrap::toDouble(point.x);
-	intersect.x = (sideConstant - pointConstant) / (wrap::toDouble(slope) - sideSlope);
-	intersect.y = intersect.x * wrap::toDouble(slope) + pointConstant;
+	intersect = {((sideConstant - pointConstant) / (wrap::toDouble(slope) - sideSlope)),
+	             intersect.getX() * wrap::toDouble(slope) + pointConstant};
   }
   else {
-	intersect.x = wrap::toDouble(point0.x);
 	auto const pointConstant =
 	    wrap::toDouble(point.y) - wrap::toDouble(slope) * wrap::toDouble(point.x);
-	intersect.y = intersect.x * wrap::toDouble(slope) + pointConstant;
+	intersect = {wrap::toDouble(point0.x), intersect.getX() * wrap::toDouble(slope) + pointConstant};
   }
   auto xMinimum = wrap::toDouble(point0.x);
   auto xMaximum = wrap::toDouble(point1.x);
   if (xMinimum > xMaximum) {
 	std::swap(xMinimum, xMaximum);
   }
-  if (delta.y != 0.0) {
+  if (delta.getY() != 0.0) {
 	auto yMinimum = wrap::toDouble(point0.y);
 	auto yMaximum = wrap::toDouble(point1.y);
 	if (yMinimum > yMaximum) {
 	  std::swap(yMinimum, yMaximum);
 	}
 	intersectionPoint = intersect;
-	return intersect.x > xMinimum && intersect.x <= xMaximum && intersect.y >= yMinimum && intersect.y <= yMaximum;
+	return intersect.getX() > xMinimum && intersect.getX() <= xMaximum &&
+	       intersect.getY() >= yMinimum && intersect.getY() <= yMaximum;
   }
   intersectionPoint = intersect;
-  return intersect.x > xMinimum && intersect.x <= xMaximum;
+  return intersect.getX() > xMinimum && intersect.getX() <= xMaximum;
 }
 
 auto form::linx(std::vector<F_POINT> const& points, uint32_t start, uint32_t finish, F_POINT& intersection) noexcept

ThrEd4/point.h
@@ -39,12 +39,14 @@ class B_SEQ_PNT;
 
 class D_POINT
 {
-  public:
-  double x {};
-  double y {};
+  private:
+  double m_xCoordinate {};
+  double m_yCoordinate {};
 
+  public:
   // constexpr D_POINT() noexcept = default;
   explicit inline D_POINT(F_POINT const& rhs) noexcept;
+  inline D_POINT(double rhsX, double rhsY) noexcept : m_xCoordinate(rhsX), m_yCoordinate(rhsY) {};
   // ToDo - Not sure why this suppression is required. CPPCheck bug?
   // cppcheck-suppress unknownMacro
   inline D_POINT(float rhsX, float rhsY) noexcept;
@@ -53,6 +55,16 @@ class D_POINT
   // D_POINT& operator=(D_POINT const& rhs) = default;
   // D_POINT& operator=(D_POINT&&) = default;
   //~D_POINT() = default;
+
+  // Getter for x
+  auto getX() const noexcept -> double {
+	return m_xCoordinate;
+  }
+
+  // Getter for y
+  auto getY() const noexcept -> double {
+	return m_yCoordinate;
+  }
 };
 
 #pragma pack(push, 1)
@@ -162,12 +174,12 @@ inline constexpr F_POINT::F_POINT(double rhsX, double rhsY) noexcept :
 }
 
 inline constexpr F_POINT::F_POINT(D_POINT const& rhs) noexcept :
-    x(gsl::narrow_cast<float>(rhs.x)), y(gsl::narrow_cast<float>(rhs.y)) {
+    x(gsl::narrow_cast<float>(rhs.getX())), y(gsl::narrow_cast<float>(rhs.getY())) {
 }
 
 inline constexpr auto F_POINT::operator=(D_POINT const& rhs) noexcept -> F_POINT& {
-  x = gsl::narrow_cast<float>(rhs.x);
-  y = gsl::narrow_cast<float>(rhs.y);
+  x = gsl::narrow_cast<float>(rhs.getX());
+  y = gsl::narrow_cast<float>(rhs.getY());
   return *this;
 }
 
@@ -202,9 +214,9 @@ inline constexpr auto F_POINT_ATTR::operator==(F_POINT_ATTR const& rhs) const no
 }
 
 inline D_POINT::D_POINT(F_POINT const& rhs) noexcept :
-    x(gsl::narrow_cast<double>(rhs.x)), y(gsl::narrow_cast<double>(rhs.y)) {
+    m_xCoordinate(gsl::narrow_cast<double>(rhs.x)), m_yCoordinate(gsl::narrow_cast<double>(rhs.y)) {
 }
 
 inline D_POINT::D_POINT(float rhsX, float rhsY) noexcept :
-    x(gsl::narrow_cast<double>(rhsX)), y(gsl::narrow_cast<double>(rhsY)) {
+    m_xCoordinate(gsl::narrow_cast<double>(rhsX)), m_yCoordinate(gsl::narrow_cast<double>(rhsY)) {
 }

commit 57dfbe58ff2f92d82159c02f56d1c8d2626e628c

Fix error introduced in decb1b9c

ThrEd4/form.cpp
@@ -2020,13 +2020,14 @@ auto fi::proj(F_POINT const& point, float slope, F_POINT const& point0, F_POINT 
 	auto const sideConstant = wrap::toDouble(point0.y) - sideSlope * wrap::toDouble(point0.x);
 	auto const pointConstant =
 	    wrap::toDouble(point.y) - wrap::toDouble(slope) * wrap::toDouble(point.x);
-	intersect = D_POINT {((sideConstant - pointConstant) / (wrap::toDouble(slope) - sideSlope)),
-	                     intersect.x * wrap::toDouble(slope) + pointConstant};
+	auto const newX = (sideConstant - pointConstant) / (wrap::toDouble(slope) - sideSlope);
+	intersect       = D_POINT {newX, newX * wrap::toDouble(slope) + pointConstant};
   }
   else {
 	auto const pointConstant =
 	    wrap::toDouble(point.y) - wrap::toDouble(slope) * wrap::toDouble(point.x);
-	intersect = D_POINT {wrap::toDouble(point0.x), intersect.x * wrap::toDouble(slope) + pointConstant};
+	auto const newX = wrap::toDouble(point0.x);
+	intersect       = D_POINT {newX, newX * wrap::toDouble(slope) + pointConstant};
   }
   auto xMinimum = wrap::toDouble(point0.x);
   auto xMaximum = wrap::toDouble(point1.x);