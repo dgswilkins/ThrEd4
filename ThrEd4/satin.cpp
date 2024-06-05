83288a908b1eca3a19eb0cf2fbff15bd6eefaa98
dgswilkins <dgswilkins@users.noreply.github.com>
2024-06-05 13:13:31 -0400

    avoid returning a redundant boolean literal

--- ThrEd4/satin.cpp
+++ ThrEd4/satin.cpp
@@ -1488,15 +1488,12 @@ void si::outfn(FRM_HEAD const& form,
 
 auto si::chkbak(std::vector<F_POINT> const& satinBackup, F_POINT const& pnt) noexcept -> bool {
   auto const lenCheck      = LineSpacing * LineSpacing;
-  if (std::ranges::any_of(satinBackup, [&](auto const& backup) {
+  return std::ranges::any_of(satinBackup, [&](auto const& backup) {
 	    auto const deltaX = backup.x - pnt.x;
 	    auto const deltaY = backup.y - pnt.y;
 	    auto const length = deltaX * deltaX + deltaY * deltaY;
 	    return length < lenCheck;
-      })) {
-	return true;
-  }
-  return false;
+  });
 }
 
 auto satin::adsatk(uint32_t count) -> uint32_t {
