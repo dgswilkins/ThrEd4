dgswilkins/ThrEd4

commit 01d52baac1e62da29abf842b8dec2115a0b82253

iterate more efficiently in frmcalc

ThrEd4/thred.cpp
@@ -2332,9 +2332,9 @@ void thi::frmcalc(uint32_t& largestStitchIndex, uint32_t& smallestStitchIndex) {
   auto       maxLength = 0.0F;
   auto       minLength = BIGFLOAT;
   if (!StitchBuffer->empty()) {
+	auto stitch     = StitchBuffer->begin();
+	auto stitchFwd1 = std::next(stitch);
 	for (auto iStitch = 0U; iStitch < endStitch; ++iStitch) {
-	  auto const stitch     = wrap::next(StitchBuffer->begin(), iStitch);
-	  auto const stitchFwd1 = std::next(stitch);
 	  if ((stitch->attribute & FRMSK) != code || ((stitch->attribute & NOTFRM) != 0U) ||
 	      (stitchFwd1->attribute & FRMSK) != code || ((stitchFwd1->attribute & TYPMSK) == 0U)) {
 		continue;
@@ -2348,6 +2348,8 @@ void thi::frmcalc(uint32_t& largestStitchIndex, uint32_t& smallestStitchIndex) {
 		minLength           = length;
 		smallestStitchIndex = iStitch;
 	  }
+	  ++stitch;
+	  ++stitchFwd1;
 	}
   }
   constexpr auto DIGITLIM = 10000.0F; // value that represents the max width that can be displayed

commit 3939bfa1a46d1f75d44d0e9cf29a04c131e1ceaa

fix error introduced in 01d52baa

ThrEd4/thred.cpp
@@ -2306,6 +2306,8 @@ void thi::frmcalc(uint32_t& largestStitchIndex, uint32_t& smallestStitchIndex) {
 	for (auto iStitch = START; iStitch < endStitch; ++iStitch) {
 	  if ((stitch->attribute & FRMSK) != code || ((stitch->attribute & NOTFRM) != 0U) ||
 	      (stitchFwd1->attribute & FRMSK) != code || ((stitchFwd1->attribute & TYPMSK) == 0U)) {
+		++stitch;
+		++stitchFwd1;
 		continue;
 	  }
 	  auto const deltaX = stitchFwd1->x - stitch->x;