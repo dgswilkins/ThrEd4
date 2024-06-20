824503acba96ccc067deccf1d7d13897947695fe
dgswilkins <dgswilkins@users.noreply.github.com>
2024-06-20 14:55:52 -0400

    remove commented out code in trace.cpp

--- ThrEd4/trace.cpp
+++ ThrEd4/trace.cpp
@@ -96,13 +96,6 @@ class TRACE_PNT
   public:
   int16_t x {};
   int16_t y {};
-
-  // constexpr TRACE_PNT() noexcept = default;
-  // TRACE_PNT(TRACE_PNT const&) = default;
-  // TRACE_PNT(TRACE_PNT&&) = default;
-  // TRACE_PNT& operator=(TRACE_PNT const& rhs) = default;
-  // TRACE_PNT& operator=(TRACE_PNT&&) = default;
-  //~TRACE_PNT() = default;
 };
 
 // trace internal namespace
