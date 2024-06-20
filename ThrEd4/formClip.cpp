46d131cfa70a6a603b8d301e74e15c715ce8f756
dgswilkins <dgswilkins@users.noreply.github.com>
2024-06-20 15:00:32 -0400

    remove commented out code in formClip

--- ThrEd4/formClip.cpp
+++ ThrEd4/formClip.cpp
@@ -65,13 +65,6 @@ class FORM_CLIP // form data clipboard header
   uint32_t clipType {};
   uint32_t reserved {};
   FRM_HEAD form;
-
-  // constexpr FORM_CLIP() noexcept = default;
-  // FORM_CLIP(FORM_CLIP const&) = default;
-  // FORM_CLIP(FORM_CLIP&&) = default;
-  // FORM_CLIP& operator=(FORM_CLIP const& rhs) = default;
-  // FORM_CLIP& operator=(FORM_CLIP&&) = default;
-  //~FORM_CLIP() = default;
 };
 #pragma pack(pop)
 
@@ -82,13 +75,6 @@ class FORMS_CLIP // multiple forms clipboard header
   uint32_t clipType {};
   uint16_t formCount {};
   uint16_t reserved {};
-
-  // constexpr FORMS_CLIP() noexcept = default;
-  // FORMS_CLIP(FORMS_CLIP const&) = default;
-  // FORMS_CLIP(FORMS_CLIP&&) = default;
-  // FORMS_CLIP& operator=(FORMS_CLIP const& rhs) = default;
-  // FORMS_CLIP& operator=(FORMS_CLIP&&) = default;
-  //~FORMS_CLIP() = default;
 };
 #pragma pack(pop)
 
@@ -99,13 +85,6 @@ class FORM_VERTEX_CLIP // form points clipboard header
   uint32_t clipType {};
   uint32_t vertexCount {};
   bool     direction {};
-
-  // constexpr FORM_VERTEX_CLIP() noexcept = default;
-  // FORM_VERTEX_CLIP(FORM_VERTEX_CLIP const&) = default;
-  // FORM_VERTEX_CLIP(FORM_VERTEX_CLIP&&) = default;
-  // FORM_VERTEX_CLIP& operator=(FORM_VERTEX_CLIP const& rhs) = default;
-  // FORM_VERTEX_CLIP& operator=(FORM_VERTEX_CLIP&&) = default;
-  //~FORM_VERTEX_CLIP() = default;
 };
 #pragma pack(pop)
 
